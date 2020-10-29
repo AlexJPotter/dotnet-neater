28c2281dabb7aed1ff0dde68eeebd2e0b85a1615
Alex Potter <alex.potter@ghyston.com>
2020-10-29 16:14:38 +0000

    Basic solution and project parsing

--- DotnetNeater.CLI/Program.cs
+++ DotnetNeater.CLI/Program.cs
@@ -1,36 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
+using Microsoft.Build.Construction;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Operations;
 
 namespace DotnetNeater.CLI
 {
     public static class Program
     {
+        private static readonly IEnumerable<IFormattingRule> RulesToApply = new[]
+        {
+            new TrimTrailingWhitespaceRule(),
+        };
+
         public static async Task Main(string[] args)
         {
-            const string inputFilePath = "C:/workspace/innovation/SampleApplication/SampleApplication/Program.cs";
+            Console.WriteLine();
+            Console.WriteLine("*** dotnet-neater ***");
+            Console.WriteLine();
 
-            var inputFileContents = await File.ReadAllTextAsync(inputFilePath);
-            Console.Write("INPUT:\r\n" + "--------------------\r\n" + inputFileContents.WithVisibleWhitespace() + "--------------------" + "\r\n\r\n\r\n");
+            var currentDirectory = Environment.CurrentDirectory;
 
-            var syntaxTree = CSharpSyntaxTree.ParseText(inputFileContents);
-            var rootNode = (CSharpSyntaxNode) await syntaxTree.GetRootAsync();
+            var files = Directory.GetFiles(currentDirectory).Select(filePath => new FileInfo(filePath)).ToList();
 
-            var rulesToApply = new[]
+            var solutionFiles = files.Where(f => f.Extension == ".sln").ToList();
+
+            if (!solutionFiles.Any())
             {
-                new TrimTrailingWhitespaceRule(),
-            };
+                await Console.Error.WriteLineAsync("Could not find a solution file");
+                return;
+            }
 
-            rootNode = rulesToApply.Aggregate(rootNode, (current, rule) => rule.ApplyToNode(current));
+            if (solutionFiles.Count > 1)
+            {
+                await Console.Error.WriteLineAsync("Found multiple solution files");
+                return;
+            }
 
-            var outputFileContents = rootNode.GetText().ToString();
-            Console.Write("OUTPUT:\r\n" + "--------------------\r\n" + outputFileContents.WithVisibleWhitespace() + "--------------------\r\n\r\n");
+            var solutionFile = SolutionFile.Parse(solutionFiles.Single().FullName);
+
+            var projects = solutionFile.ProjectsInOrder.ToList();
+
+            // TODO - Do this more intelligently
+            var ignoredDirectories = new[] { "\\bin\\", "\\obj\\" };
+
+            var filesToFormat = projects
+                .SelectMany(project =>
+                {
+                    var projectDirectory = Directory.GetParent(project.AbsolutePath);
+
+                    var cSharpFiles = projectDirectory.EnumerateFiles("*.cs", SearchOption.AllDirectories)
+                        .Where(f => !ignoredDirectories.Any(d => f.FullName.Contains(d)))
+                        .ToList();
+
+                    return cSharpFiles;
+                })
+                .Select(fileInfo => fileInfo.FullName)
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine($"Found {filesToFormat.Count} C# files to format ...\r\n");
+            await Task.Delay(2000);
+
+            foreach (var filePath in filesToFormat)
+            {
+                await FormatFile(filePath);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static async Task FormatFile(string filePath)
+        {
+            var oldFileContents = await File.ReadAllTextAsync(filePath);
+
+            var oldSyntaxTree = CSharpSyntaxTree.ParseText(oldFileContents);
+            var oldRootNode = (CSharpSyntaxNode) await oldSyntaxTree.GetRootAsync();
+
+            var newRootNode = RulesToApply.Aggregate(oldRootNode, (current, rule) => rule.ApplyToNode(current));
+            var newSyntaxTree = CSharpSyntaxTree.Create(newRootNode);
+
+            var hasChanged = newSyntaxTree.GetChanges(oldSyntaxTree).Any();
+
+            if (hasChanged)
+            {
+                var newFileContents = (await newSyntaxTree.GetTextAsync()).ToString();
+                await File.WriteAllTextAsync(filePath, newFileContents);
+            }
+
+            var originalConsoleColour = Console.ForegroundColor;
+            Console.ForegroundColor = hasChanged ? ConsoleColor.White : ConsoleColor.DarkGray;
+            Console.WriteLine(filePath);
+            Console.ForegroundColor = originalConsoleColour;
         }
     }
 }
